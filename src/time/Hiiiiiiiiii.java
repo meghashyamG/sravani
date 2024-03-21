a5de2d2575042bf822d501e7c32abaa85cd06a48
shyam <shyamgodavari7@gmail.com>
2024-03-21 17:22:21 +0530

    commit

--- src/time/Hiiiiiiiiii.java
+++ src/time/Hiiiiiiiiii.java
@@ -5,6 +5,8 @@ public class Hiiiiiiiiii {
 	public static void main(String[] args) {
 		System.out.println("saskeeeeeeeeeeee!!!!!!!!!!!!!!!");
 		System.out.println("narutoooooooooooooooooo!!!!!!!!!!");
+		System.out.println("rasengan!!!!!!!!!!!!");
+		
 	}
 	
 	
